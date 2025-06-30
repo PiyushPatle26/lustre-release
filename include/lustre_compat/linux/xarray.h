603f1c60e648de4ae6fd3e91b14a87f67ccfe628
Piyush Patle <nikhilgpatle60@gmail.com>
2025-06-30 22:11:29 +0530

    Fix XArray helper redefinition errors for modern kernels
    - Wrapped all compatibility XArray helper functions in include/lustre_compat/linux/xarray.h
      with a single #ifdef HAVE_XARRAY_SUPPORT guard.
    - Now, if the kernel provides XArray helpers, none of the compatibility functions are defined,
      preventing all redefinition errors when building against recent kernels.
    - Removed individual #ifndef guards for each function for clarity and maintainability.

--- include/lustre_compat/linux/xarray.h
+++ include/lustre_compat/linux/xarray.h
@@ -15,252 +15,77 @@
  *
  * See Documentation/core-api/xarray.rst for how to use the XArray.
  */
-#ifndef HAVE_XARRAY_SUPPORT
-
-#if defined(NEED_LOCKDEP_IS_HELD_DISCARD_CONST) \
- && defined(CONFIG_LOCKDEP) \
- && defined(lockdep_is_held)
-#undef lockdep_is_held
-	#define lockdep_is_held(lock) \
-		lock_is_held((struct lockdep_map *)&(lock)->dep_map)
-#endif
-
-#include <linux/bug.h>
-#include <linux/compiler.h>
-#include <linux/gfp.h>
-#include <linux/kconfig.h>
-#include <linux/kernel.h>
-#include <linux/rcupdate.h>
-#include <linux/spinlock.h>
-#include <linux/types.h>
-
-/*
- * The bottom two bits of the entry determine how the XArray interprets
- * the contents:
- *
- * 00: Pointer entry
- * 10: Internal entry
- * x1: Value entry or tagged pointer
- *
- * Attempting to store internal entries in the XArray is a bug.
- *
- * Most internal entries are pointers to the next node in the tree.
- * The following internal entries have a special meaning:
- *
- * 0-62: Sibling entries
- * 256: Zero entry
- * 257: Retry entry
- *
- * Errors are also represented as internal entries, but use the negative
- * space (-4094 to -2).  They're never stored in the slots array; only
- * returned by the normal API.
- */
-
-#define BITS_PER_XA_VALUE	(BITS_PER_LONG - 1)
-
-/**
- * xa_mk_value() - Create an XArray entry from an integer.
- * @v: Value to store in XArray.
- *
- * Context: Any context.
- * Return: An entry suitable for storing in the XArray.
- */
-#ifndef xa_mk_value
+#ifdef HAVE_XARRAY_SUPPORT
+// Kernel provides all xarray helpers, do not define any compatibility functions
+#else
 static inline void *xa_mk_value(unsigned long v)
 {
 	WARN_ON((long)v < 0);
 	return (void *)((v << 1) | 1);
 }
-#endif
 
-/**
- * xa_to_value() - Get value stored in an XArray entry.
- * @entry: XArray entry.
- *
- * Context: Any context.
- * Return: The value stored in the XArray entry.
- */
-#ifndef xa_to_value
 static inline unsigned long xa_to_value(const void *entry)
 {
 	return (unsigned long)entry >> 1;
 }
-#endif
 
-/**
- * xa_is_value() - Determine if an entry is a value.
- * @entry: XArray entry.
- *
- * Context: Any context.
- * Return: True if the entry is a value, false if it is a pointer.
- */
-#ifndef xa_is_value
 static inline bool xa_is_value(const void *entry)
 {
 	return (unsigned long)entry & 1;
 }
-#endif
 
-/**
- * xa_tag_pointer() - Create an XArray entry for a tagged pointer.
- * @p: Plain pointer.
- * @tag: Tag value (0, 1 or 3).
- *
- * If the user of the XArray prefers, they can tag their pointers instead
- * of storing value entries.  Three tags are available (0, 1 and 3).
- * These are distinct from the xa_mark_t as they are not replicated up
- * through the array and cannot be searched for.
- *
- * Context: Any context.
- * Return: An XArray entry.
- */
-#ifndef xa_tag_pointer
 static inline void *xa_tag_pointer(void *p, unsigned long tag)
 {
 	return (void *)((unsigned long)p | tag);
 }
-#endif
 
-/**
- * xa_untag_pointer() - Turn an XArray entry into a plain pointer.
- * @entry: XArray entry.
- *
- * If you have stored a tagged pointer in the XArray, call this function
- * to get the untagged version of the pointer.
- *
- * Context: Any context.
- * Return: A pointer.
- */
-#ifndef xa_untag_pointer
 static inline void *xa_untag_pointer(void *entry)
 {
 	return (void *)((unsigned long)entry & ~3UL);
 }
-#endif
 
-/**
- * xa_pointer_tag() - Get the tag stored in an XArray entry.
- * @entry: XArray entry.
- *
- * If you have stored a tagged pointer in the XArray, call this function
- * to get the tag of that pointer.
- *
- * Context: Any context.
- * Return: A tag.
- */
-#ifndef xa_pointer_tag
 static inline unsigned int xa_pointer_tag(void *entry)
 {
 	return (unsigned long)entry & 3UL;
 }
-#endif
 
-/*
- * xa_mk_internal() - Create an internal entry.
- * @v: Value to turn into an internal entry.
- *
- * Internal entries are used for a number of purposes.  Entries 0-255 are
- * used for sibling entries (only 0-62 are used by the current code).  256
- * is used for the retry entry.  257 is used for the reserved / zero entry.
- * Negative internal entries are used to represent errnos.  Node pointers
- * are also tagged as internal entries in some situations.
- *
- * Context: Any context.
- * Return: An XArray internal entry corresponding to this value.
- */
-#ifndef xa_mk_internal
 static inline void *xa_mk_internal(unsigned long v)
 {
 	return (void *)((v << 2) | 2);
 }
-#endif
 
-/*
- * xa_to_internal() - Extract the value from an internal entry.
- * @entry: XArray entry.
- *
- * Context: Any context.
- * Return: The value which was stored in the internal entry.
- */
-#ifndef xa_to_internal
 static inline unsigned long xa_to_internal(const void *entry)
 {
 	return (unsigned long)entry >> 2;
 }
-#endif
 
-/*
- * xa_is_internal() - Is the entry an internal entry?
- * @entry: XArray entry.
- *
- * Context: Any context.
- * Return: %true if the entry is an internal entry.
- */
-#ifndef xa_is_internal
 static inline bool xa_is_internal(const void *entry)
 {
 	return ((unsigned long)entry & 3) == 2;
 }
-#endif
 
-#define XA_ZERO_ENTRY		xa_mk_internal(257)
+#define XA_ZERO_ENTRY xa_mk_internal(257)
 
-/**
- * xa_is_zero() - Is the entry a zero entry?
- * @entry: Entry retrieved from the XArray
- *
- * The normal API will return NULL as the contents of a slot containing
- * a zero entry.  You can only see zero entries by using the advanced API.
- *
- * Return: %true if the entry is a zero entry.
- */
-#ifndef xa_is_zero
 static inline bool xa_is_zero(const void *entry)
 {
 	return unlikely(entry == XA_ZERO_ENTRY);
 }
-#endif
 
-/**
- * xa_is_err() - Report whether an XArray operation returned an error
- * @entry: Result from calling an XArray function
- *
- * If an XArray operation cannot complete an operation, it will return
- * a special value indicating an error.  This function tells you
- * whether an error occurred; xa_err() tells you which error occurred.
- *
- * Context: Any context.
- * Return: %true if the entry indicates an error.
- */
-#ifndef xa_is_err
 static inline bool xa_is_err(const void *entry)
 {
 	return unlikely(xa_is_internal(entry) &&
 			entry >= xa_mk_internal(-MAX_ERRNO));
 }
-#endif
 
-/**
- * xa_err() - Turn an XArray result into an errno.
- * @entry: Result from calling an XArray function.
- *
- * If an XArray operation cannot complete an operation, it will return
- * a special pointer value which encodes an errno.  This function extracts
- * the errno from the pointer value, or returns 0 if the pointer does not
- * represent an errno.
- *
- * Context: Any context.
- * Return: A negative errno or 0.
- */
-#ifndef xa_err
 static inline int xa_err(void *entry)
 {
-	/* xa_to_internal() would not do sign extension. */
 	if (xa_is_err(entry))
 		return (long)entry >> 2;
 	return 0;
 }
 #endif
+
+#define BITS_PER_XA_VALUE	(BITS_PER_LONG - 1)
 
 /**
  * struct xa_limit - Represents a range of IDs.
@@ -1786,8 +1611,4 @@ static inline void *xas_next(struct xa_state *xas)
 	xas->xa_offset++;
 	return xa_entry(xas->xa, node, xas->xa_offset);
 }
-#else
-#include <linux/xarray.h>
-#endif /* !HAVE_XARRAY_SUPPORT */
-
 #endif /* _LINUX_XARRAY_LUSTRE_H */
